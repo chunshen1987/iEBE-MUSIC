082a5f3182ea9ebee881ab5059c93a14719bd44b
Chun Shen <chunshen1987@gmail.com>
2019-02-08 11:00:23 -0500

    add a script to fetch an IP-Glasma event as MUSIC initial condition from the hdf5 database

--- IP-Glasma_database/combine_events_into_hdf5.py
+++ IP-Glasma_database/combine_events_into_hdf5.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
+"""
+     This script combines pre-generated IP-Glasma events into a hdf5 database.
+"""
 
 import sys
 import h5py
@@ -10,11 +13,12 @@ import numpy as np
 def print_help():
     print("{0} results_folder".format(sys.argv[0]))
 
-if (len(sys.argv) < 2):
+try:
+    results_folder = str(sys.argv[1])
+except IndexError:
     print_help()
     exit(1)
 
-results_folder = str(sys.argv[1])
 results_name = results_folder.split("/")[-1]
 if results_name == "":
     results_name = results_folder.split("/")[-2]
@@ -35,7 +39,7 @@ for ievent, event_path in enumerate(event_list):
                                     compression="gzip", compression_opts=9)
         if "epsilon-u-Hydro" in file_name:
             f = open(file_path)
-            header = f.readline()
+            header = f.readline().strip('\n')
             dset.attrs.create("header", np.string_(header))
 
 hf.close()
