556655965bb29f0c3e9c3b0b5e6129eda3afb4b0
Chun Shen <chunshen1987@gmail.com>
2019-02-09 11:58:01 -0500

    move fetching IP-Glasma events to running time

--- codes/hydro_plus_UrQMD_driver.py
+++ codes/hydro_plus_UrQMD_driver.py
@@ -6,26 +6,33 @@ from os import path, mkdir
 from glob import glob
 import sys
 import shutil
+from fetch_IPGlasma_event_from_hdf5_database import fecth_an_IPGlasma_event
 
 def print_Usage():
-    print("Usage: {} n_UrQMD_ev ".format(sys.argv[0]))
+    print("Usage: {} ".format(sys.argv[0]) + "initial_condition_database "
+          + "n_hydro_events hydro_event_id n_threads")
 
 
-def get_initial_condition():
-    filelist = glob("Initial/*")
-    for ifile in filelist:
-        yield(ifile)
+def get_initial_condition(database, nev, idx0):
+    time_stamp_str = "0.4"
+    for iev in range(idx0, idx0 + nev):
+        file_name = fecth_an_IPGlasma_event(database, time_stamp_str, iev)
+        yield(file_name)
     
 
 def run_UrQMD_event(event_id):
     call("bash ./run_afterburner.sh {0:d}".format(event_id), shell=True)
 
 
-def main(n_UrQMD_events):
+def main(initial_condition_database, n_hydro_events, hydro_event_id0,
+         n_threads) :
     print("Number of processors: ", cpu_count())
     
-    mkdir("event_results")
-    for ifile in get_initial_condition():
+    final_results_folder="EVENT_RESLUTS"
+    mkdir(final_results_folder)
+
+    for ifile in get_initial_condition(initial_condition_database,
+                                       n_hydro_events, hydro_event_id0):
         print("Run simulations with {} ... ".format(ifile))
         event_id = ifile.split("/")[-1].split("-")[-1].split(".dat")[0]
         shutil.move(ifile, "MUSIC/initial/epsilon-u-Hydro.dat")
@@ -35,20 +42,25 @@ def main(n_UrQMD_events):
         call("bash ./run_hydro.sh", shell=True)
 
         # collect hydro results
-        shutil.move("MUSIC/hydro_results",
-                    "event_results/hydro_results_{}".format(event_id))
+        hydro_folder_name = "hydro_results_{}".format(event_id)
+        shutil.move("MUSIC/hydro_results", path.join(final_results_folder,
+                                                     hydro_folder_name))
         
         # then run UrQMD events in parallel
         print("Running UrQMD ... ")
-        with Pool(processes=n_UrQMD_events) as pool:
-            pool.map(run_UrQMD_event, range(n_UrQMD_events))
+        with Pool(processes=n_threads) as pool:
+            pool.map(run_UrQMD_event, range(n_threads))
 
 
 if __name__ == "__main__":
     try:
-        n_UrQMD_events = int(sys.argv[1])
+        initial_condition_database = str(sys.argv[1])
+        n_hydro_events             = int(sys.argv[2])
+        hydro_event_id0            = int(sys.argv[3])
+        n_threads                  = int(sys.argv[4])
     except IndexError:
         print_Usage()
         exit(0)
 
-    main(n_UrQMD_events)
+    main(initial_condition_database, n_hydro_events, hydro_event_id0,
+         n_threads)
--- generate_jobs.py
+++ generate_jobs.py
@@ -6,10 +6,10 @@ import shutil
 from glob import glob
 import subprocess
 import random
-from IPGlasma_database.fetch_IPGlasma_event_from_hdf5_database import fecth_an_IPGlasma_event
 
 
-def write_script_header(cluster, script, event_id, walltime, working_folder):
+def write_script_header(cluster, script, n_threads,
+                        event_id, walltime, working_folder):
     if cluster == "nersc":
         script.write(
 """#!/bin/bash -l
@@ -24,39 +24,39 @@ def write_script_header(cluster, script, event_id, walltime, working_folder):
         script.write(
 """#!/usr/bin/env bash
 #PBS -N event_{0:s}
-#PBS -l nodes=1:ppn=1
-#PBS -l walltime={1:s}
+#PBS -l nodes=1:ppn={1:d}
+#PBS -l walltime={2:s}
 #PBS -S /bin/bash
 #PBS -e test.err
 #PBS -o test.log
 #PBS -A cqn-654-ad
 #PBS -q sw
-#PBS -d {2:s}
-""".format(event_id, walltime, working_folder))
+#PBS -d {3:s}
+""".format(event_id, n_threads, walltime, working_folder))
     elif cluster == "McGill":
         script.write(
 """#!/usr/bin/env bash
 #PBS -N event_{0:s}
-#PBS -l nodes=1:ppn=1:irulan
-#PBS -l walltime={1:s}
+#PBS -l nodes=1:ppn={1:d}:irulan
+#PBS -l walltime={2:s}
 #PBS -S /bin/bash
 #PBS -e test.err
 #PBS -o test.log
-#PBS -d {2:s}
-""".format(event_id, walltime, working_folder))
+#PBS -d {3:s}
+""".format(event_id, n_threads, walltime, working_folder))
     elif cluster == "wsugrid":
         script.write(
 """#!/usr/bin/env bash
 #PBS -N event_{0:s}
-#PBS -l select=1:ncpus=1:mem=5GB
-#PBS -l walltime={1:s}
+#PBS -l select=1:ncpus={1:d}:mem=5GB
+#PBS -l walltime={2:s}
 #PBS -S /bin/bash
 #PBS -e test.err
 #PBS -o test.log
 #PBS -q wsuq
 
-cd {2:s}
-""".format(event_id, walltime, working_folder))
+cd {3:s}
+""".format(event_id, n_threads, walltime, working_folder))
     elif cluster == "local":
         script.write("#!/usr/bin/env bash")
     else:
@@ -65,10 +65,24 @@ cd {2:s}
         exit(1)
 
 
-def generate_script_hydro(cluster_name, folder_name, nthreads):
+def generate_full_job_script(cluster_name, folder_name,
+                             database, n_hydro, ev0_id, n_threads):
     working_folder = folder_name
     event_id = working_folder.split('/')[-1]
     walltime = '35:00:00'
+
+    script = open(path.join(working_folder, "submit_job.pbs"), "w")
+    write_script_header(cluster_name, script, n_threads, event_id, walltime,
+                        working_folder)
+    script.write(
+"""
+    ./hydro_plus_UrQMD_driver.py {0:s} {1:d} {2:d} {3:d}
+""".format(database, n_hydro, ev0_id, n_threads))
+    script.close()
+
+
+def generate_script_hydro(folder_name, nthreads):
+    working_folder = folder_name
 
     script = open(path.join(working_folder, "run_hydro.sh"), "w")
 
@@ -80,23 +94,19 @@ def generate_script_hydro(cluster_name, folder_name, nthreads):
 results_folder={0:s}
 
 export OMP_NUM_THREADS={1:d}
-export OMP_PROC_BIND=spread
-export OMP_PLACES=threads
 
 (
 cd MUSIC
 # hydro evolution
-./mpihydro music_input_mode_2 1> mode_2.log 2> mode_2.err
+./mpihydro music_input_mode_2
 ./sweeper.sh $results_folder
 )
 """.format(hydro_results_folder, ppn))
     script.close()
 
 
-def generate_script_afterburner(cluster_name, folder_name):
+def generate_script_afterburner(folder_name):
     working_folder = folder_name
-    event_id = working_folder.split('/')[-1]
-    walltime = '35:00:00'
 
     script = open(path.join(working_folder, "run_afterburner.sh"), "w")
     script.write(
@@ -118,17 +128,17 @@ do
     mkdir results
     mv ../hydro_events/$iev results/surface.dat
     cp ../hydro_events/music_input_event_$event_id results/music_input
-    ./iSS.e >> ../output.log
+    ./iSS.e
     mv results/surface.dat ../hydro_events/$iev
     #rm -fr results/sample*
     # turn on global momentum conservation
     ./correct_momentum_conservation.py OSCAR.DAT
     mv OSCAR_w_GMC.DAT OSCAR.DAT
     cd ../osc2u
-    ./osc2u.e < ../iSS/OSCAR.DAT >> ../output.log
+    ./osc2u.e < ../iSS/OSCAR.DAT
     mv fort.14 ../urqmd/OSCAR.input
     cd ../urqmd
-    ./runqmd.sh >> ../output.log
+    ./runqmd.sh
     mv particle_list.dat ../UrQMD_results/particle_list_$event_id.dat
     rm -fr ../iSS/OSCAR.DAT
     rm -fr OSCAR.input
@@ -154,14 +164,12 @@ def generate_event_folders(initial_condition_database, working_folder,
     shutil.copy('codes/hydro_plus_UrQMD_driver.py', event_folder)
     shutil.copy('IPGlasma_database/fetch_IPGlasma_event_from_hdf5_database.py',
                 event_folder)
-    initial_folder = path.join(event_folder, 'Initial')
-    mkdir(initial_folder)
-    for iev in range(event_id*n_hydro_per_job, (event_id + 1)*n_hydro_per_job):
-        copy_IPGlasma_initial_condition(initial_condition_database, iev,
-                                        initial_folder)
-    generate_script_hydro(cluster_name, event_folder, n_UrQMD_per_hydro)
+    generate_full_job_script(cluster_name, event_folder,
+                             initial_condition_database, n_hydro_per_job,
+                             event_id*n_hydro_per_job, n_UrQMD_per_hydro)
+    generate_script_hydro(event_folder, n_UrQMD_per_hydro)
     shutil.copytree('codes/MUSIC', path.join(event_folder, 'MUSIC'))
-    generate_script_afterburner(cluster_name, event_folder)
+    generate_script_afterburner(event_folder)
     for iev in range(n_UrQMD_per_hydro):
         sub_event_folder = path.join(working_folder,
                                      'event_{0:d}'.format(event_id),
@@ -189,7 +197,8 @@ if __name__ == "__main__":
         print_Usage()
         exit(0)
 
-    working_folder_name = path.abspath(working_folder_name)
+    initial_condition_database = path.abspath(initial_condition_database)
+    working_folder_name        = path.abspath(working_folder_name)
     mkdir(working_folder_name)
     for iev in range(n_jobs):
         generate_event_folders(initial_condition_database, working_folder_name,
