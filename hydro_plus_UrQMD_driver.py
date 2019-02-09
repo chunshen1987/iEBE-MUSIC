a0f9bfd8f7379aed5b2225537b26e754a3c271c6
Chun Shen <chunshen1987@gmail.com>
2019-02-09 00:43:18 -0500

    use Pool to parallel UrQMD runs after hydro

--- hydro_plus_UrQMD_driver.py
+++ hydro_plus_UrQMD_driver.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python
 
-from multiprocessing import Process
+from multiprocessing import Pool, cpu_count
 from subprocess import call
 import sys
-import os
 
 def run_UrQMD_event(event_id):
     call("bash ./run_afterburner.sh {0:d}".format(event_id), shell=True)
 
-n_UrQMD_events = int(sys.argv[1])
-
-# first run hydro
-#call("bash ./run_hydro.sh", shell=True)
-
-# then run UrQMD events in parallel
-for i in range(n_UrQMD_events):
-    p = Process(target=run_UrQMD_event, args=(i,))
-    p.start()
-    p.join()
+if __name__ == "__main__":
+    print("Number of processors: ", cpu_count())
+    n_UrQMD_events = int(sys.argv[1])
+    
+    # first run hydro
+    print("Playing MUSIC ... ")
+    call("bash ./run_hydro.sh", shell=True)
+    
+    # then run UrQMD events in parallel
+    print("Running UrQMD ... ")
+    with Pool(processes=n_UrQMD_events) as pool:
+        pool.map(run_UrQMD_event, range(n_UrQMD_events))
