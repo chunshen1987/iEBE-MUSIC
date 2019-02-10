3fbd63d9b8c3d66eecdbd38514079a51d9e6e082
Chun Shen <chunshen1987@gmail.com>
2019-02-10 10:43:39 -0500

    add job submission script for NERSC

---
+++ NERSC_supports/job_MPI_wrapper.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from mpi4py import MPI
+from subprocess import call
+import sys
+from os import chdir
+
+def print_Usage():
+    print("Usage: {} n_threads job_id0 ".format(sys.argv[0]))
+
+try:
+    n_threads = int(sys.argv[1])
+    job_id    = int(sys.argv[2])
+except IndexError:
+    print_Usage()
+    exit(0)
+
+comm = MPI.COMM_WORLD
+size = comm.Get_size()
+rank = comm.Get_rank()
+
+event_id = rank + n_threads*(job_id - 1)
+chdir("event_{}".format(event_id))
+call("bash submmit_job.pbs", shell=True)
