830f5dd1faf77d42f1e5522791ea9fddd61b38ab
Chun Shen <chunshen1987@gmail.com>
2019-02-15 10:08:38 -0500

    improve the collecting script for the IP-Glasma initial conditions

--- IPGlasma_database/collect_IPGlasma_files_into_events.sh
+++ IPGlasma_database/collect_IPGlasma_files_into_events.sh
@@ -1,12 +1,22 @@
 #!/usr/bin/env bash
 
-nev=10
 event_folder=$1
 
-cd $event_folder
-for (( i=0; i < $nev; i++ ))
-do
-    mkdir event-$i
-    mv epsilon-u-Hydro-t0.4-$i.dat event-$i
-done
-cd -
+nev=0
+tflag="t0.4"
+
+(
+    cd $event_folder
+    
+    for ifile in `ls | grep "epsilon-u-Hydro"`
+    do
+        event_id=`echo $ifile | sed 's/.dat//' | cut -f 5 -d "-"`
+        if [[ -f epsilon-u-Hydro-$tflag-$event_id.dat ]]
+        then
+            mkdir event-$event_id
+            mv epsilon-u-Hydro-$tflag-$event_id.dat event-$event_id
+            nev=$((nev + 1))
+        fi
+    done
+    echo "There are $nev events in total."
+)
