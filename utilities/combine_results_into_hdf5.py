38f2af4c8bf6967534d33c975abcece0d758f9f3
Chun Shen <chunshen1987@gmail.com>
2019-05-08 14:02:27 -0400

    fixed the warnings from linter

--- utilities/combine_results_into_hdf5.py
+++ utilities/combine_results_into_hdf5.py
@@ -1,32 +1,33 @@
 #!/usr/bin/env python
+"""This script combine multiple hdf5 data files to one"""
 
 import sys
-import h5py
 from os import path, system
 from glob import glob
+import h5py
 
-import numpy as np
 
 def print_help():
+    """This function outpus help messages"""
     print("{0} results_folder".format(sys.argv[0]))
 
-if (len(sys.argv) < 2):
+if len(sys.argv) < 2:
     print_help()
     exit(1)
 
-results_folder = str(sys.argv[1])
-results_name = results_folder.split("/")[-1]
-if results_name == "":
-    results_name = results_folder.split("/")[-2]
-results_path = path.abspath(path.join(".", results_folder))
-event_list = glob(path.join(results_path, "*.h5"))
+RESULTS_FOLDER = str(sys.argv[1])
+RESULTS_NAME = RESULTS_FOLDER.split("/")[-1]
+if RESULTS_NAME == "":
+    RESULTS_NAME = RESULTS_FOLDER.split("/")[-2]
+RESULTS_PATH = path.abspath(path.join(".", RESULTS_FOLDER))
+EVENT_LIST = glob(path.join(RESULTS_PATH, "*.h5"))
 
-for ievent, event_path in enumerate(event_list):
+for ievent, event_path in enumerate(EVENT_LIST):
     print("processing {0} ... ".format(event_path))
     event_name = event_path.split("/")[-1]
     hftemp = h5py.File(event_path, "r")
-    glist  = list(hftemp.keys())
+    glist = list(hftemp.keys())
     hftemp.close()
     for igroup, gtemp in enumerate(glist):
         system('h5copy -i {0} -o {1}.h5 -s {2} -d {2}'.format(
-                                            event_path, results_name, gtemp))
+            event_path, RESULTS_NAME, gtemp))
