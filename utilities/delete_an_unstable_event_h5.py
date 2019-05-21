255c162b47b43978883de2bb81988b3f83192749
Chun Shen <chunshen1987@gmail.com>
2019-05-21 09:39:59 -0400

    add a help script to delete an unstable event from the hdf5 database file

---
+++ utilities/delete_an_unstable_event_h5.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python
+"""
+    This script delete an unstable event in the final collected hdf5 database
+"""
+
+from sys import argv
+from os import path
+import h5py
+
+def main():
+    """This is the main function"""
+    try:
+        data_h5 = path.abspath(argv[1])
+        event_id = int(argv[2])
+    except IndexError:
+        print("Usage: {} database_filename delete_event_id".format(
+            str(argv[0])))
+        exit(1)
+    h5_file = h5py.File(data_h5, "a")
+    print("deleting event {} from {} ...".format(event_id, data_h5))
+    del h5_file['spvn_results_{}'.format(event_id)]
+    h5_file.close()
+
+
+if __name__ == "__main__":
+    main()
