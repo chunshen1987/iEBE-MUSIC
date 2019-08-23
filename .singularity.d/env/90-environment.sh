648e5225bfa3d8a15d6d24c7ec46d316e4af3a24
Chun Shen <chunshen1987@gmail.com>
2019-08-23 11:34:38 -0400

    take out the singularity commands, they are not effective

--- .singularity.d/env/90-environment.sh
+++
@@ -1,8 +0,0 @@
-# Custom environment shell code should follow
-
-if [ "x$LD_LIBRARY_PATH" = "x" ]; then
-    export LD_LIBRARY_PATH="/host-libs"
-else
-    export LD_LIBRARY_PATH="/host-libs:$LD_LIBRARY_PATH"
-fi
-
