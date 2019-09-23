8ae9dd0c9a3e4af72b3a97b675af1048256bc2eb
Chun Shen <chunshen1987@gmail.com>
2019-09-23 17:24:17 -0400

    check both get_group and get_file

--- IPGlasma_database/fetch_IPGlasma_event_from_hdf5_database.py
+++ IPGlasma_database/fetch_IPGlasma_event_from_hdf5_database.py
@@ -20,13 +20,13 @@ def fecth_an_IPGlasma_event_Tmunu(database_path, time_stamp, event_idx):
     )
     hf          = h5py.File(database_path, "r")
     event_name  = "event-{0:d}".format(event_idx)
+    file_name   = "Tmunu-t{0:s}-{1:d}.dat".format(time_stamp, event_idx)
     try:
         event_group = hf.get(event_name)
+        temp_data   = event_group.get(file_name)
     except AttributeError:
         print("Can not load {}".format(event_name))
         return("Failed")
-    file_name   = "Tmunu-t{0:s}-{1:d}.dat".format(time_stamp, event_idx)
-    temp_data   = event_group.get(file_name)
     data_header = temp_data.attrs["header"].decode('UTF-8').replace('#','')
     x_size      = temp_data.attrs["x_size"]
     y_size      = temp_data.attrs["y_size"]
@@ -56,14 +56,14 @@ def fecth_an_IPGlasma_event(database_path, time_stamp, event_idx):
     )
     hf          = h5py.File(database_path, "r")
     event_name  = "event-{0:d}".format(event_idx)
+    file_name   = "epsilon-u-Hydro-t{0:s}-{1:d}.dat".format(time_stamp,
+                                                            event_idx)
     try:
         event_group = hf.get(event_name)
+        temp_data   = event_group.get(file_name)
     except AttributeError:
         print("Can not load {}".format(event_name))
         return("Failed")
-    file_name   = "epsilon-u-Hydro-t{0:s}-{1:d}.dat".format(time_stamp,
-                                                            event_idx)
-    temp_data   = event_group.get(file_name)
     data_header = temp_data.attrs["header"].decode('UTF-8').replace('#','')
     x_size      = temp_data.attrs["x_size"]
     y_size      = temp_data.attrs["y_size"]
