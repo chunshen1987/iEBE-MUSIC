da19eee1e06eeebe99b066891fceac7e1893bb2a
Chun Shen <chunshen1987@gmail.com>
2020-01-12 12:34:57 -0500

    collect rapidity dependence vn with a different pT cut

--- generate_jobs.py
+++ generate_jobs.py
@@ -332,6 +332,7 @@ pid=$1
     cd hadronic_afterburner_toolkit
     if [ "$pid" == "9999" ]; then
         # charged hadrons
+        ./hadronic_afterburner_tools.e particle_monval=$pid distinguish_isospin=0 rap_type=0 rap_min=-0.5 rap_max=0.5 compute_correlation=0 flag_charge_dependence=0 vn_rapidity_dis_pT_min=0.15 vn_rapidity_dis_pT_max=3.0 > run.log
         ./hadronic_afterburner_tools.e particle_monval=$pid distinguish_isospin=0 rap_type=0 rap_min=-0.5 rap_max=0.5 compute_correlation=0 flag_charge_dependence=0 > run.log
         ./hadronic_afterburner_tools.e particle_monval=$pid distinguish_isospin=0 rap_type=0 rap_min=-1.0 rap_max=-0.1 compute_correlation=0 flag_charge_dependence=0 >> run.log
         ./hadronic_afterburner_tools.e particle_monval=$pid distinguish_isospin=0 rap_type=0 rap_min=0.1 rap_max=1.0 compute_correlation=0 flag_charge_dependence=0 >> run.log
