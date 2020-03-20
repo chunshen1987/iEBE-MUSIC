0472c34e97e4f20550816e99197d9630ffb50540
Chun Shen <chunshen1987@gmail.com>
2020-03-20 08:30:50 -0700

    some small updates

--- codes/get_code_packages_chun.sh
+++ codes/get_code_packages_chun.sh
@@ -5,6 +5,9 @@
 # download 3DMCGlauber
 git clone --depth=1 https://github.com/chunshen1987/3dMCGlauber -b remnant 3dMCGlauber_code
 
+# download KoMPoST
+git clone --depth=1 https://github.com/j-f-paquet/kompost kompost_code
+
 # download MUSIC
 git clone https://Chunshen1987@bitbucket.org/Chunshen1987/music_dev.git -b new_string_format MUSIC_code
 
@@ -16,3 +19,12 @@ git clone --depth=1 https://Chunshen1987@bitbucket.org/Chunshen1987/urqmd_afterb
 
 # download hadronic afterner
 git clone --depth=1 https://github.com/chunshen1987/hadronic_afterburner_toolkit hadronic_afterburner_toolkit_code
+
+
+# download neos_bqs
+neos_bqs_name="neosBQS-v0.11"
+neos_bqs_destination="MUSIC_code/EOS/"
+wget https://s3.amazonaws.com/qcdneos/${neos_bqs_name}.tar.gz && \
+tar -zxf ${neos_bqs_name}.tar.gz -C ${neos_bqs_destination} && \
+mv ${neos_bqs_destination}/${neos_bqs_name} ${neos_bqs_destination}/neos_bqs && \
+rm ${neos_bqs_name}.tar.gz
