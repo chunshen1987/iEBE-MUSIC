61b86ea4809c2497e3b8fb02b8b0d488e9c8bfe5
Chun Shen <chunshen1987@gmail.com>
2020-03-24 11:18:00 -0400

    add some helper script for WSUGrid

---
+++ Cluster_supports/WSUgrid/cancel_running_jobs.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+usage="./cancel_running_jobs.sh fromFolder toFolder"
+
+jobFolder=$1
+
+if [ -z "$jobFolder" ]
+then
+    echo $usage
+    exit 1
+fi
+
+jobFolder=${jobFolder%"/"}
+
+echo "cancelling jobs in " ${jobFolder}
+
+for ijob in `ls --color=none ${jobFolder} | grep "event" `;
+do
+    eventsPath=${jobFolder}/${ijob}
+    qdel `cat ${eventsPath}/job_id`
+done
+
