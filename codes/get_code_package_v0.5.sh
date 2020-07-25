da74d78d76b4f408ec8116cd7376026edf151cba
Chun Shen <chunshen1987@gmail.com>
2020-07-25 15:53:16 -0400

    add a script to download the fixed version of code packages

---
+++ codes/get_code_package_v0.5.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# download the code package
+
+# download 3DMCGlauber
+git clone --depth=1 --branch v0.5 https://github.com/chunshen1987/3dMCGlauber 3dMCGlauber_code
+
+# download MUSIC
+git clone --depth=1 --branch v2.5 https://github.com/MUSIC-fluid/MUSIC MUSIC_code
+
+# download iSS particle sampler
+git clone --depth=1 --branch v1.0 https://github.com/chunshen1987/iSS iSS_code
+
+# download UrQMD afterburner
+git clone --depth=1 https://Chunshen1987@bitbucket.org/Chunshen1987/urqmd_afterburner.git urqmd_code
+
+# download hadronic afterner
+git clone --depth=1 --branch v1.0 https://github.com/chunshen1987/hadronic_afterburner_toolkit hadronic_afterburner_toolkit_code
+
+# download neos_bqs
+neos_bqs_name="neosBQS-v0.11"
+neos_bqs_destination="MUSIC_code/EOS/"
+wget https://s3.amazonaws.com/qcdneos/${neos_bqs_name}.tar.gz && \
+tar -zxf ${neos_bqs_name}.tar.gz -C ${neos_bqs_destination} && \
+mv ${neos_bqs_destination}/${neos_bqs_name} ${neos_bqs_destination}/neos_bqs && \
+rm ${neos_bqs_name}.tar.gz
