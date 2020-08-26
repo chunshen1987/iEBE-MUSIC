290579dbffa6fae342b278af7438e13fa89ea25d
Chun Shen <chunshen1987@gmail.com>
2020-08-26 11:30:55 -0400

    use the new convension on WSUgrid

--- Cluster_supports/WSUgrid/submit_all_jobs.sh
+++ Cluster_supports/WSUgrid/submit_all_jobs.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# available queues are: primary, requeue, express, debug, secondary, gpu
 
 usage="./submite_all_jobs.sh workFolder [queue_name]"
 
@@ -16,24 +17,6 @@ then
     queue="primary"
 fi
 
-partition=$SPRIMARY
-if [ "$queue" == "requeue" ]; then
-    partition=$SREQUEUE
-fi
-if [ "$queue" == "debug" ]; then
-    partition=$SDEBUG
-fi
-if [ "$queue" == "secondary" ]; then
-    partition=$SSECONDARY
-fi
-if [ "$queue" == "gpu" ]; then
-    partition=$SGPU
-fi
-if [ "$queue" == "express" ]; then
-    partition=$SEXPRESS
-fi
-echo $partition
-
 echo "submit jobs in " ${workFolder}
 echo ${workFolder} >> current_sims_list.txt
 
@@ -43,7 +26,7 @@ for ijob in `ls --color=none | grep "event"`;
 do
     echo "submit job in " ${workFolder}/${ijob}
     cd ${ijob}
-    sbatch -q ${queue} -p ${partition} submit_job.pbs | awk {'print $4'} > job_id
+    sbatch -q ${queue} submit_job.pbs | awk {'print $4'} > job_id
     cd ..
     ((Numjobs++))
 done
