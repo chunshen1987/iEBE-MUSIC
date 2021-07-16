afe856394a3b4f2b564a82be47bea67e8d87cc73
Chun Shen <chunshen1987@gmail.com>
2021-07-16 11:21:04 -0400

    add checking while compile the code packages

--- codes/compile_code_packages.sh
+++ codes/compile_code_packages.sh
@@ -22,6 +22,10 @@ echo -e "${Green}compile 3dMCGlauber ... ${NC}"
     make -j${number_of_cores_to_compile}
     make install
 )
+status=$?
+if [ $status -ne 0 ]; then
+    exit $status
+fi
 mkdir -p 3dMCGlauber
 cp 3dMCGlauber_code/input 3dMCGlauber/
 
@@ -31,6 +35,10 @@ echo -e "${Green}compile IPGlasma ... ${NC}"
     cd ipglasma_code
     ./compile_IPGlasma.sh
 )
+status=$?
+if [ $status -ne 0 ]; then
+    exit $status
+fi
 mkdir -p ipglasma
 cp ipglasma_code/input ipglasma/
 
@@ -40,6 +48,10 @@ echo -e "${Green}compile KoMPoST ... ${NC}"
     cd kompost_code
     make
 )
+status=$?
+if [ $status -ne 0 ]; then
+    exit $status
+fi
 mkdir -p kompost
 cp kompost_code/setup.ini kompost/
 
@@ -53,6 +65,10 @@ echo -e "${Green}compile MUSIC ... ${NC}"
     make -j${number_of_cores_to_compile}
     make install
 )
+status=$?
+if [ $status -ne 0 ]; then
+    exit $status
+fi
 mkdir -p MUSIC
 cp MUSIC_code/example_inputfiles/IPGlasma_2D/music_input_mode_2 MUSIC/
 cp MUSIC_code/utilities/sweeper.sh MUSIC/
@@ -68,6 +84,10 @@ echo -e "${Green}compile iSS ... ${NC}"
     make -j${number_of_cores_to_compile}
     make install
 )
+status=$?
+if [ $status -ne 0 ]; then
+    exit $status
+fi
 mkdir -p iSS
 cp -r iSS_code/iSS_parameters.dat iSS/
 
@@ -77,6 +97,10 @@ echo -e "${Green}compile UrQMD ... ${NC}"
     cd urqmd_code
     make -j${number_of_cores_to_compile}
 )
+status=$?
+if [ $status -ne 0 ]; then
+    exit $status
+fi
 mkdir -p osc2u
 cp urqmd_code/osc2u/osc2u.e osc2u/
 mkdir -p urqmd
@@ -99,6 +123,10 @@ echo -e "${Green}compile hadronic afterburner toolkit ... ${NC}"
     g++ concatenate_binary_files.cpp -lz -o concatenate_binary_files.e
     mv concatenate_binary_files.e ../
 )
+status=$?
+if [ $status -ne 0 ]; then
+    exit $status
+fi
 mkdir -p hadronic_afterburner_toolkit
 cp hadronic_afterburner_toolkit_code/convert_to_binary.e hadronic_afterburner_toolkit/
 cp hadronic_afterburner_toolkit_code/concatenate_binary_files.e hadronic_afterburner_toolkit/
