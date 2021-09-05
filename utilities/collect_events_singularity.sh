b69508aa6650bbf64ed1970774630e783de7b3d6
Chun Shen <chunshen1987@gmail.com>
2021-09-05 17:27:13 -0400

    update the collecting script for singularity results

--- utilities/collect_events_singularity.sh
+++ utilities/collect_events_singularity.sh
@@ -25,19 +25,25 @@ folderName=`echo $fromFolder | rev | cut -d "/" -f 1 | rev`
 target_folder=${toFolder}/${folderName}
 mkdir -p ${target_folder}
 cp ${fromFolder}/parameters_dict_*.py ${target_folder}/
+target_hydro_folder=${target_folder}/HYDRO_RESULTS
+mkdir -p ${target_hydro_folder}
+target_urqmd_folder=${target_folder}/URQMD_RESULTS
+mkdir -p ${target_urqmd_folder}
 target_spvn_folder=${target_folder}/SPVN_RESULTS
 mkdir -p ${target_spvn_folder}
 
+hydro_folder_name="hydro_results_"
+UrQMD_file_name="particle_list_"
 spvn_folder_name="spvn_results_"
 
-total_eventNum=0
-collected_eventNum=0
 for ijob in `ls --color=none $fromFolder | grep "event" `;
 do
     eventsPath=${fromFolder}/${ijob}
     for iev in `ls --color=none ${eventsPath} | grep "RESULTS"`
     do
         mv ${eventsPath}/${iev} $target_spvn_folder
+        mv ${eventsPath}/temp/playground/HYDRO_RESULTS/${hydro_folder_name}* $target_hydro_folder
+        mv ${eventsPath}/temp/playground/URQMD_RESULTS/${UrQMD_file_name}* $target_urqmd_folder
     done
 done
 
