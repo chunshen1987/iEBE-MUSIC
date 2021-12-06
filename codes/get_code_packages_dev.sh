a7d2ef137af2572ab560eee4cdc673313040eb21
Wenbin <1501110084@pku.edu.cn>
2021-12-05 19:03:48 -0500

    change eta cut to fit ATLAS UPC data

--- codes/get_code_packages_dev.sh
+++ codes/get_code_packages_dev.sh
@@ -34,6 +34,6 @@ rm -fr urqmd_code/.git
 
 # download hadronic afterner
 rm -fr hadronic_afterburner_toolkit_code
-git clone --depth=1 https://github.com/chunshen1987/hadronic_afterburner_toolkit hadronic_afterburner_toolkit_code
+git clone --depth=1 https://github.com/chunshen1987/hadronic_afterburner_toolkit --branch UPC hadronic_afterburner_toolkit_code
 rm -fr hadronic_afterburner_toolkit_code/.git
 
