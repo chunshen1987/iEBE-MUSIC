4d6bea1d7fd868c0a7d2d422432fe4220b5e8131
Chun Shen <chunshen1987@gmail.com>
2022-05-19 11:19:30 -0400

    update the scripts

--- codes/get_code_packages.sh
+++ codes/get_code_packages.sh
@@ -3,30 +3,37 @@
 # download the code package
 
 # download 3DMCGlauber
+rm -fr 3dMCGlauber_code
 git clone --depth=1 https://github.com/chunshen1987/3dMCGlauber 3dMCGlauber_code
+rm -fr 3dMCGlauber_code/.git
 
 # download IPGlasma
+rm -fr ipglasma_code
 git clone --depth=1 https://github.com/chunshen1987/ipglasma ipglasma_code
+rm -fr ipglasma_code/.git
 
 # download KoMPoST
+rm -fr kompost_code
 git clone --depth=1 https://github.com/j-f-paquet/kompost kompost_code
+rm -fr kompost_code/.git
 
 # download MUSIC
-git clone --depth=1 https://github.com/MUSIC-fluid/MUSIC MUSIC_code
+rm -fr MUSIC_code
+git clone --depth=1 https://github.com/MUSIC-fluid/MUSIC -b v3.1 MUSIC_code
+rm -fr MUSIC_code/.git
 
 # download iSS particle sampler
-git clone --depth=1 https://github.com/chunshen1987/iSS iSS_code
+rm -fr iSS_code
+git clone --depth=1 https://github.com/chunshen1987/iSS -b v1.1 iSS_code
+rm -fr iSS_code/.git
 
 # download UrQMD afterburner
+rm -fr urqmd_code
 git clone --depth=1 https://Chunshen1987@bitbucket.org/Chunshen1987/urqmd_afterburner.git urqmd_code
+rm -fr urqmd_code/.git
 
 # download hadronic afterner
+rm -fr hadronic_afterburner_toolkit_code
 git clone --depth=1 https://github.com/chunshen1987/hadronic_afterburner_toolkit hadronic_afterburner_toolkit_code
+rm -fr hadronic_afterburner_toolkit_code/.git
 
-# download neos_bqs
-neos_bqs_name="neosBQS-v0.11"
-neos_bqs_destination="MUSIC_code/EOS/"
-wget https://s3.amazonaws.com/qcdneos/${neos_bqs_name}.tar.gz && \
-tar -zxf ${neos_bqs_name}.tar.gz -C ${neos_bqs_destination} && \
-mv ${neos_bqs_destination}/${neos_bqs_name} ${neos_bqs_destination}/neos_bqs && \
-rm ${neos_bqs_name}.tar.gz
