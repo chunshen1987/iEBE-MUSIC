7faf3b7021843b65ce6dbffd0b445b75bed5ae65
Chun <chunshen@wayne.edu>
2023-04-19 11:21:30 -0400

    update the utility script

--- utilities/get_Qnvectors.sh
+++ utilities/get_Qnvectors.sh
@@ -6,7 +6,8 @@ database=${2%"/"}
 for iev in `ls --color=none ${evfolder}`
 do
     eventid=`echo $iev | sed 's/hydro_results_//' | sed 's$/$$'`
-    ./fetch_Qnvectors_from_hdf5_database.py $database $eventid
+    python3 fetch_Qnvectors_from_hdf5_database.py $database $eventid
     mv Qn_vectors_${eventid}.dat ${evfolder}/$iev/
     mv particle_yield_and_meanpT_${eventid}.dat ${evfolder}/$iev/
+    mv Ncoll* ${evfolder}/$iev/
 done
