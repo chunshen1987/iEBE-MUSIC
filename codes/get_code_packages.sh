475ee0d9c03d4c1313fd1cef88c801187a47d868
Chun Shen <chunshen@wayne.edu>
2024-02-19 18:15:15 -0500

    update the iSS version for runnig with pdg particle list

--- codes/get_code_packages.sh
+++ codes/get_code_packages.sh
@@ -29,7 +29,7 @@ rm -fr MUSIC_code/.git
 # download iSS particle sampler
 rm -fr iSS_code
 git clone --depth=1 https://github.com/chunshen1987/iSS -b dev iSS_code
-(cd iSS_code; git checkout b19766ec566b278308654d37462cbade9b941f0e)
+(cd iSS_code; git checkout 3b151fdd03f5cd0f41c19b11af68e379c4c5f571)
 rm -fr iSS_code/.git
 
 # download photonEmission wrapper
