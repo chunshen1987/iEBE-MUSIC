5367ece1cafc3ff347a0fc4e3cc797d3598b26dd
Chun Shen <chunshen@wayne.edu>
2024-04-12 16:53:15 -0400

    fixed a typo

--- analysisKit/average_event_spin_h5_minimumbias.py
+++ analysisKit/average_event_spin_h5_minimumbias.py
@@ -1014,7 +1014,7 @@ def analyze_Rspin_y(hf_, eventList_, outputFolder_: str,
                           /mean(array(dNList[ifile]), axis=0))
         R_spin_err.append(std(array(RspinList[ifile]), axis=0)
                           /mean(array(dNList[ifile]), axis=0)/sqrt(nev))
-    fileName = f"averaged_Rspin_{vorticityType}_pT_{pTMin_}_{pTmax_}.txt"
+    fileName = f"averaged_Rspin_{vorticityType}_pT_{pTmin_}_{pTmax_}.txt"
     f = open(path.join(outputFolder_, fileName), "w")
     f.write("# eta  Rspin  Rspin_err "
             + "({0}  {0}+SIP(BBPP)  {0}+SIP(LY)  {0}+SIP+MuBIP(LY))\n".format(
