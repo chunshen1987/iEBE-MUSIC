646ab08d37a358d59f27847aca03dd3544a1aebc
Chun Shen <chunshen1987@gmail.com>
2024-05-30 14:26:24 -0400

    update the fetch EoS script

--- utilities/Pick_EOS_From_File.py
+++ utilities/Pick_EOS_From_File.py
@@ -2,31 +2,32 @@ import sys
 import pickle
 
 # main function should accept a command line argument for the index of the EoS to be picked
-def main():
-
-    # get a command line argument for the index of the EoS to be picked
-    if len(sys.argv) != 3:
-        print("Usage: Pick_EOS_From_File.py filename index")
-        sys.exit(1)
-
-    filename = str(sys.argv[1])
-    index = f"{int(sys.argv[2]):04d}"
-    print(f"Pick_EOS_From_File: filename = {filename}, index = {index}")
+def fetch_an_EOS(database: str, id: int) -> None:
+    index = f"{id:04d}"
+    print(f"Pick_EOS_From_File: filename = {database}, index = {index}")
 
     # load the EoS file
-    with open(filename, 'rb') as f:
+    with open(database, 'rb') as f:
         EoS_dict = pickle.load(f)
 
     # pick the EoS with the given index
     if index in EoS_dict:
         data = EoS_dict[index]
     else:
-        print(f"Index {index} not found in file {filename}")
+        print(f"Index {index} not found in file {database}")
         sys.exit(1)
 
     # write the EoS to a new binary file with columns e, P, T
     with open(f"EoS_{index}.bin", 'wb') as f:
         f.write(data.tobytes())
 
+
 if __name__ == "__main__":
-    main()
+    try:
+        filename = str(sys.argv[1])
+        index = int(sys.argv[2])
+    except IndexError:
+        print("Usage: Pick_EOS_From_File.py filename index")
+        sys.exit(1)
+
+    fetch_an_EOS(filename, index)
