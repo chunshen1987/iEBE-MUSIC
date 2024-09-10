2063ff570d68d43e958e644be427ec5c276185af
Chun Shen <chunshen@wayne.edu>
2024-09-10 14:02:46 -0400

    update the script to combine multiple pickle files

--- analysisKit/combine_pickle_files.py
+++ analysisKit/combine_pickle_files.py
@@ -7,39 +7,34 @@ import random
 
 
 def help_message():
-    print("Usage: {0} dataFile1 dataFile2".format(sys.argv[0]))
-    print("merge dataFile2 into dataFile1")
+    print("Usage: {0} dataFile1 dataFile2 ...".format(sys.argv[0]))
+    print("merge dataFile2 ... into dataFile1")
     exit(0)
 
 
-def randomString(stringLength=1):
-    """Generate a random string of fixed length """
-    letters = string.ascii_lowercase
-    return ''.join(random.choice(letters) for i in range(stringLength))
-
-
-try:
-    database_file1 = str(sys.argv[1])
-    database_file2 = str(sys.argv[2])
-except IndexError:
+if len(sys.argv) < 3:
     help_message()
 
+database_file1 = str(sys.argv[1])
+database_fileList = [str(i) for i in sys.argv[2:]]
 
 with open(database_file1, "rb") as pf:
     data1 = pickle.load(pf)
+print(f"File {database_file1} has {len(list(data1.keys()))} entries ...")
 
-with open(database_file2, "rb") as pf:
-    data2 = pickle.load(pf)
+letters = string.ascii_lowercase
+for i, database_file2 in enumerate(database_fileList):
+    keyList = set(data1.keys())
+    with open(database_file2, "rb") as pf:
+        data2 = pickle.load(pf)
+    print(f"File {database_file2} has {len(list(data2.keys()))} entries ...")
+    key2 = set(data2.keys())
+    if len(keyList.intersection(key2)) > 0:
+        addLetter = letters[i]
+        for key_i in list(data2.keys()):
+            newKey = f"{key_i}_{addLetter}"
+            data2[newKey] = data2.pop(key_i)
+    data1 = data1 | data2
 
-keyList = list(data1.keys())
-for key_i in list(data2.keys()):
-    keyName = key_i
-    while keyName in keyList:
-        randomlabel = randomString()
-        keyName = f"{keyName}{randomlabel}"
-    data1[keyName] = data2[key_i]
-    keyList.append(keyName)
-
-fileName = "combined_{}".format(database_file1.split("/")[-1])
-with open(fileName, "wb") as pf:
+with open("combined.pkl", "wb") as pf:
     pickle.dump(data1, pf)
