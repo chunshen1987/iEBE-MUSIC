7f29c63d6709f0ee6540351722a62f1df38c6921
Chun Shen <chunshen1987@gmail.com>
2024-09-11 21:11:48 -0400

    update the EOS Generator together with viscosity Generator

---
+++ EOS_database/download_QGPViscositydatabase1D.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1nYypKHPt7Y7mx5GXwP2jK7ZKIPDR4_Xb' -O eta_s.pkl
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1e_CXlhlMY7pON8fUBCIQbV5oc5_mamYt' -O zeta_s.pkl
