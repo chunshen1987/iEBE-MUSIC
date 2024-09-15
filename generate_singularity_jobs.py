c1129c7cdf4cdcc0e06b6675b65d9dd840a454f4
Chun Shen <chunshen1987@gmail.com>
2024-09-15 11:51:01 -0400

    supress error messages from bash

--- generate_singularity_jobs.py
+++ generate_singularity_jobs.py
@@ -160,7 +160,7 @@ def generate_event_folders(workingFolder, clusterName, eventId,
     write_script_header(clusterName, script, nThreads, eventId, wallTime,
                         eventFolder)
     script.write("""
-h5Stat=`ls *.h5`
+h5Stat=`ls *.h5 2>/dev/null`
 
 if [ -z "$h5Stat" ]
 then
