63e762e17a32e347238e5551ee3ea66dd1396e48
Chun Shen <chunshen@wayne.edu>
2024-09-30 17:58:57 -0400

    update the download link for EoS Table because google drive link is not stable
    Use the Dropbox link instead

--- EOS_database/download_EOSdatabase1D.sh
+++ EOS_database/download_EOSdatabase1D.sh
@@ -2,4 +2,6 @@
 
 #wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1rT3ymUIFqjUgyBjqdQrVSltoXQUdE7b0' -O EoS.pkl
 #wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1cq78a9LXpGGwRsJ4pIj6AZO8S76WKtuY' -O EoS.pkl
-wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1iBZDyp20IJHxHyvCvoLOEg_C0Q2ypifm' -O EoS.pkl
+#wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1iBZDyp20IJHxHyvCvoLOEg_C0Q2ypifm' -O EoS.pkl
+
+wget --no-check-certificate 'https://www.dropbox.com/scl/fi/pxm2fv24mzgirx3nevsud/EoS.pkl?rlkey=pbb5rzx2869z8hhxgblerpgh7&dl=0' -O EoS.pkl
