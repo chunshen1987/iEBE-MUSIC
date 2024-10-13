a9e4d1c5c41b40f2413b0dd3439b45b9e25d4afe
Chun Shen <chunshen1987@gmail.com>
2024-10-13 19:52:06 -0400

    update the EOS and viscosity pickle files

--- EOS_database/download_QGPViscositydatabase1D.sh
+++ EOS_database/download_QGPViscositydatabase1D.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
-wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1nYypKHPt7Y7mx5GXwP2jK7ZKIPDR4_Xb' -O eta_s.pkl
-wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1e_CXlhlMY7pON8fUBCIQbV5oc5_mamYt' -O zeta_s.pkl
+# v1.0
+#wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1nYypKHPt7Y7mx5GXwP2jK7ZKIPDR4_Xb' -O eta_s.pkl
+#wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1e_CXlhlMY7pON8fUBCIQbV5oc5_mamYt' -O zeta_s.pkl
+
+# v1.1
+wget --no-check-certificate 'https://www.dropbox.com/scl/fi/1tpjf510646rjnzkpme73/eta_s.pkl?rlkey=vftdzpsusvy0beb8dfw4tdmjq&dl=0' -O eta_s.pkl
+wget --no-check-certificate 'https://www.dropbox.com/scl/fi/zqiv8ni1bnkcjr1nf4r1j/zeta_s.pkl?rlkey=g7lsvge7i0fkjxb14v2vg27eb&dl=0' -O zeta_s.pkl
+
