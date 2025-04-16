7d35d38592aa3900e2611746dc60104badbc8446
Chun Shen <chunshen1987@gmail.com>
2025-04-16 09:08:57 +0200

    update the MUSIC version for new treatments when resumTransportCoeff is on
    We regulate the viscous tensor of the IP-Glasma initial condition before
    the evolution.

--- codes/get_code_packages.sh
+++ codes/get_code_packages.sh
@@ -23,7 +23,7 @@ rm -fr kompost_code/.git
 # download MUSIC
 rm -fr MUSIC_code
 git clone --depth=3 https://github.com/MUSIC-fluid/MUSIC -b chun_dev MUSIC_code
-(cd MUSIC_code; git checkout ee4efcdcc06eae9c8be7c0f72e1ecbb1037972fe)
+(cd MUSIC_code; git checkout 9b88f37eddfe88714dd9a392dc29c65a90892ffc)
 rm -fr MUSIC_code/.git
 
 # download iSS particle sampler
