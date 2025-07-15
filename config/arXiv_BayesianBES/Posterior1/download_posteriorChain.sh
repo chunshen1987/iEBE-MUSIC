59e928d9cfba1535c7b3d4c492089d872f000c55
Chun Shen <chunshen@wayne.edu>
2025-07-15 11:17:04 -0400

    update the download link to the official Zenodo repo

--- config/arXiv_BayesianBES/Posterior1/download_posteriorChain.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-wget --no-check-certificate 'https://www.dropbox.com/scl/fi/zxp1rueujqpr4zmia0wb7/posteriorChain_arXiv2408.00537.pkl?rlkey=n34ydi8357ntt8ipv4poaz313&dl=0' -O posteriorChain.pkl
--- config/arXiv_BayesianBES/download_posteriorChains.sh
+++ config/arXiv_BayesianBES/download_posteriorChains.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-cd Posterior1
-bash download_posteriorChain.sh
-
-cd ../Posterior2
-bash download_posteriorChain.sh
-
-cd ../Posterior3
-bash download_posteriorChain.sh
+curl --output temp.tar.gz "https://zenodo.org/api/records/15920131/files/posterior_chains.tar.gz/content"
+tar -xf temp.tar.gz
+rm temp.tar.gz
+mv PosteriorChains/Posterior1/posteriorChain.pkl Posterior1/
+mv PosteriorChains/Posterior2/posteriorChain.pkl Posterior2/
+mv PosteriorChains/Posterior3/posteriorChain.pkl Posterior3/
+rm -fr PosteriorChains
