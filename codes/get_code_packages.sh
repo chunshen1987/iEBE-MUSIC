8e64a0476e6a9c1ae1ad91f9a4c2d1ac8361679c
Chun Shen <chunshen@wayne.edu>
2025-07-18 11:14:50 -0400

    update the IP-Glasma version

--- codes/get_code_packages.sh
+++ codes/get_code_packages.sh
@@ -11,7 +11,7 @@ rm -fr 3dMCGlauber_code/.git
 # download IPGlasma
 rm -fr ipglasma_code
 git clone --depth=1 https://github.com/chunshen1987/ipglasma -b ipglasma_jimwlk ipglasma_code
-(cd ipglasma_code; git checkout 7cd9f37a38181fec85b71399ac7b46f233200a97)
+(cd ipglasma_code; git checkout e37b9f191c3e5be00edb3c298348a6c7aeb73e6a)
 rm -fr ipglasma_code/.git
 
 # download KoMPoST
