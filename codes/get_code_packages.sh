64eaada11e381b7671b75790057c84e87ca95110
Chun Shen <chunshen@wayne.edu>
2025-10-29 16:35:07 -0400

    update MUSIC version

--- codes/get_code_packages.sh
+++ codes/get_code_packages.sh
@@ -23,7 +23,7 @@ rm -fr kompost_code/.git
 # download MUSIC
 rm -fr MUSIC_code
 git clone --depth=3 https://github.com/MUSIC-fluid/MUSIC -b chun_dev MUSIC_code
-(cd MUSIC_code; git checkout a3572dd7aa3dc0985f634f967b4b87a044e48663)
+(cd MUSIC_code; git checkout ab6fbacbb01f1fd0532798f1d1032993f5f466a1)
 rm -fr MUSIC_code/.git
 
 # download iSS particle sampler
