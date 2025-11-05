dd4b72f82d4ff58d4a9bcd2f3f27b86ec76c5281
Chun Shen <chunshen@wayne.edu>
2025-11-05 15:48:54 -0500

    update MUSIC for Tmunu source term

--- codes/get_code_packages.sh
+++ codes/get_code_packages.sh
@@ -23,7 +23,7 @@ rm -fr kompost_code/.git
 # download MUSIC
 rm -fr MUSIC_code
 git clone --depth=3 https://github.com/MUSIC-fluid/MUSIC -b chun_dev MUSIC_code
-(cd MUSIC_code; git checkout 7fb890930ffb2d8d98f2a480419864803168e50f)
+(cd MUSIC_code; git checkout 505e6912c3b078fcd121f258b14e17cd6e3f3187)
 rm -fr MUSIC_code/.git
 
 # download iSS particle sampler
