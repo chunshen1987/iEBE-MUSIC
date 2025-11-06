c9ef733f27a8644ee24b7d20b71a1ff852533b3d
Hendrik Roch <hroch@physik.uni-bielefeld.de>
2025-11-06 11:26:33 -0500

    Fix issue that files can not be copied to symlink directory

--- generate_jobs.py
+++ generate_jobs.py
@@ -3,7 +3,7 @@
 
 import sys
 import re
-from os import path, mkdir, remove
+from os import path, mkdir, remove, listdir, makedirs, symlink
 import shutil
 import subprocess
 import argparse
@@ -655,7 +655,19 @@ def generate_event_folders(initial_condition_database, initial_condition_type,
     subprocess.call(f"ln -s {targetFile} {desLoc}", shell=True)
     targetFile = path.join(code_path, 'MUSIC_code/EOS')
     desLoc = path.join(event_folder, "MUSIC/EOS")
-    subprocess.call(f"ln -s {targetFile} {desLoc}", shell=True)
+    makedirs(desLoc, exist_ok=True)
+    for item in listdir(targetFile):
+        src = path.join(targetFile, item)
+        dst = path.join(desLoc, item)
+        if path.exists(dst):
+            continue
+        try:
+            symlink(src, dst)
+        except OSError:
+            if path.isdir(src):
+                shutil.copytree(src, dst, dirs_exist_ok=True)
+            else:
+                shutil.copy2(src, dst)
     if EOSType == 42:
         eosDatabase = path.join(package_root_path, 'EOS_database', EOSFileName)
         eosFileName = fetch_an_EOS(eosDatabase, EOSId)
