6340408991a00534f6e6546466d40888115879ab
Chun Shen <chunshen@wayne.edu>
2026-02-24 10:22:54 -0500

    update the MUSIC version
    Afrid fixed the normalization for TATB initial conditions

--- codes/get_code_packages.sh
+++ codes/get_code_packages.sh
@@ -23,7 +23,7 @@ rm -fr kompost_code/.git
 # download MUSIC
 rm -fr MUSIC_code
 git clone --depth=3 https://github.com/MUSIC-fluid/MUSIC -b main MUSIC_code
-(cd MUSIC_code; git checkout d4f5def384c9695e091cc4a15a76515584057ba3)
+(cd MUSIC_code; git checkout 1024579a9fa72c2c89cfd076657955feaf0d9d0e)
 rm -fr MUSIC_code/.git
 
 # download iSS particle sampler
