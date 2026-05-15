ff6852c8842e381ef0c5591b6e4c875e04930d91
Chun Shen <chunshen@wayne.edu>
2026-05-15 09:58:16 -0400

    update MUSIC version
    use an official `main_4D` branch for MUSIC

--- codes/get_code_packages.sh
+++ codes/get_code_packages.sh
@@ -22,8 +22,8 @@ rm -fr kompost_code/.git
 
 # download MUSIC
 rm -fr MUSIC_code
-git clone --depth=3 https://github.com/MUSIC-fluid/MUSIC -b greg_dev MUSIC_code
-(cd MUSIC_code; git checkout bb7a9a957978b576c77adbe1323d7193e574a49e)
+git clone --depth=3 https://github.com/MUSIC-fluid/MUSIC -b main_4D MUSIC_code
+(cd MUSIC_code; git checkout 7e8234e988f442330c1f8f8c9982f1fe6fce638a)
 rm -fr MUSIC_code/.git
 
 # download iSS particle sampler
